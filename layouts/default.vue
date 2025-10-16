fa436a958c5175969353fc0248c2781586054576
Matin Muhammad <matinrajab.mr@gmail.com>
2025-10-16 11:38:48 +0700

    responsive sidebar for mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="flex">
+  <div class="flex min-h-screen">
     <aside
-      class="font-medium text-unselected h-screen border-r border-solid border-line-border w-72"
+      class="fixed lg:static top-0 left-0 h-screen w-64 bg-white border-r border-line-border transform transition-transform duration-300 z-40"
+      :class="
+        isSidebarOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
+      "
     >
       <h1 class="font-bold text-3xl text-secondary-text py-5 px-10">Admin</h1>
       <div class="w-fit mx-auto">
@@ -51,14 +54,27 @@
         </button>
       </div>
     </aside>
-    <div>
+
+    <div
+      v-if="isSidebarOpen"
+      class="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
+      @click="isSidebarOpen = false"
+    ></div>
+
+    <div class="flex-1 flex flex-col overflow-x-hidden">
       <header
-        class="flex justify-between items-center border-b border-solid border-line-border py-5 px-10"
+        class="flex justify-between items-center gap-4 border-b border-line-border py-5 px-6 lg:px-10 bg-white z-20"
       >
-        <h1 class="font-semibold text-3xl text-secondary-text">
-          Employee List
-        </h1>
-        <nav>
+        <div class="flex items-center gap-4">
+          <button class="lg:hidden focus:outline-none" @click="toggleSidebar">
+            <i class="material-icons text-secondary-text text-3xl">menu</i>
+          </button>
+          <h1 class="font-semibold text-2xl lg:text-3xl text-secondary-text">
+            Employee List
+          </h1>
+        </div>
+
+        <nav class="flex-1 max-w-md ml-auto">
           <div class="relative w-full">
             <i
               class="material-icons absolute left-4 top-1/2 -translate-y-1/2 text-tertiary-text"
@@ -73,7 +89,7 @@
           </div>
         </nav>
       </header>
-      <main>
+      <main class="flex-1 max-w-full overflow-x-hidden">
         <slot />
       </main>
     </div>
@@ -91,4 +107,14 @@ const { isAuthenticated, user, logout } = useSanctumAuth<User>();
 function handleLogout() {
   logout();
 }
+const isSidebarOpen = ref(false);
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+}
 </script>
+
+<style scoped>
+aside {
+  transition: transform 0.3s ease;
+}
+</style>
--- pages/employees/index.vue
+++ pages/employees/index.vue
@@ -21,59 +21,63 @@ await useAsyncData("employees", () => employeeStore.getEmployees());
     </div>
 
     <div v-else class="px-6 pb-6">
-      <table class="table-auto bg-white rounded-3xl">
-        <thead>
-          <tr class="border-b border-solid border-line-border">
-            <th class="font-normal px-2 py-1 text-xs">No</th>
-            <th class="font-normal px-2 py-1 text-xs">NIP</th>
-            <th class="font-normal px-2 py-1 text-xs">Nama</th>
-            <th class="font-normal px-2 py-1 text-xs">Tempat Lahir</th>
-            <th class="font-normal px-2 py-1 text-xs">Alamat</th>
-            <th class="font-normal px-2 py-1 text-xs">Tgl Lahir</th>
-            <th class="font-normal px-2 py-1 text-xs">L/P</th>
-            <th class="font-normal px-2 py-1 text-xs">Gol</th>
-            <th class="font-normal px-2 py-1 text-xs">Eselon</th>
-            <th class="font-normal px-2 py-1 text-xs">Jabatan</th>
-            <th class="font-normal px-2 py-1 text-xs">Tempat Tugas</th>
-            <th class="font-normal px-2 py-1 text-xs">Agama</th>
-            <th class="font-normal px-2 py-1 text-xs">Unit Kerja</th>
-            <th class="font-normal px-2 py-1 text-xs">No. HP</th>
-            <th class="font-normal px-2 py-1 text-xs">NPWP</th>
-            <th class="font-normal px-2 py-1 text-xs text-center" colspan="2">
-              Aksi
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="(employee, index) in data?.data || []"
-            :key="employee.id"
-            class="border-b border-solid border-line-border"
-          >
-            <td class="px-2 py-1 text-xs">{{ index + 1 }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.nip }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.name }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.birth_place }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.address }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.birth_date }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.gender }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.golongan }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.eselon }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.position }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.work_place }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.religion }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.work_unit }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.phone_number }}</td>
-            <td class="px-2 py-1 text-xs">{{ employee.npwp }}</td>
-            <td class="px-2 py-1 text-xs">
-              <NuxtLink to="#">Edit</NuxtLink>
-            </td>
-            <td class="px-2 py-1 text-xs text-danger">
-              <NuxtLink to="#">Delete</NuxtLink>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="overflow-x-auto rounded-3xl shadow-sm">
+        <table
+          class="table-auto bg-white min-w-full border-collapse rounded-3xl"
+        >
+          <thead>
+            <tr class="border-b border-solid border-line-border">
+              <th class="font-normal px-2 py-1 text-xs">No</th>
+              <th class="font-normal px-2 py-1 text-xs">NIP</th>
+              <th class="font-normal px-2 py-1 text-xs">Nama</th>
+              <th class="font-normal px-2 py-1 text-xs">Tempat Lahir</th>
+              <th class="font-normal px-2 py-1 text-xs">Alamat</th>
+              <th class="font-normal px-2 py-1 text-xs">Tgl Lahir</th>
+              <th class="font-normal px-2 py-1 text-xs">L/P</th>
+              <th class="font-normal px-2 py-1 text-xs">Gol</th>
+              <th class="font-normal px-2 py-1 text-xs">Eselon</th>
+              <th class="font-normal px-2 py-1 text-xs">Jabatan</th>
+              <th class="font-normal px-2 py-1 text-xs">Tempat Tugas</th>
+              <th class="font-normal px-2 py-1 text-xs">Agama</th>
+              <th class="font-normal px-2 py-1 text-xs">Unit Kerja</th>
+              <th class="font-normal px-2 py-1 text-xs">No. HP</th>
+              <th class="font-normal px-2 py-1 text-xs">NPWP</th>
+              <th class="font-normal px-2 py-1 text-xs text-center" colspan="2">
+                Aksi
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="(employee, index) in data?.data || []"
+              :key="employee.id"
+              class="border-b border-solid border-line-border"
+            >
+              <td class="px-2 py-1 text-xs">{{ index + 1 }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.nip }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.name }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.birth_place }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.address }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.birth_date }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.gender }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.golongan }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.eselon }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.position }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.work_place }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.religion }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.work_unit }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.phone_number }}</td>
+              <td class="px-2 py-1 text-xs">{{ employee.npwp }}</td>
+              <td class="px-2 py-1 text-xs">
+                <NuxtLink to="#">Edit</NuxtLink>
+              </td>
+              <td class="px-2 py-1 text-xs text-danger">
+                <NuxtLink to="#">Delete</NuxtLink>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
